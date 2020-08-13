b63d26872e2ec00860b232e92faea9165eb3d0ba
Luana Leal <luana.leal.eq@gmail.com>
2020-08-13 01:16:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog 
   def initialize(dogs_name)
-    @dog = dogs_name
+    @name = dogs_name
   end 
   
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+  
+  def name 
+    @name 
+  end 
 end 
