6ae95cece66bcf67feeeb4baa5d8bde5a463daed
Luana Leal <luana.leal.eq@gmail.com>
2020-08-13 01:29:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end 
+  
+  def name=(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end 
+  
+  def name 
+    @name 
+    @breed
+  end 
+  
+end 
+
+
+
+
+  
+ 
